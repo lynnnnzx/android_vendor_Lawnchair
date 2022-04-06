9d0710fe85730cb86946f18ef934558815a0eee7
Lynx <rifqikanekii140202@gmail.com>
2022-04-07 01:03:24 +0700

    Lawnchair: overlay: Fixup
    Signed-off-by: Lynx <rifqikanekii140202@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,11 +1,17 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
+LOCAL_PATH := vendor/lawnchair
+
 PRODUCT_COPY_FILES += \
-    vendor/lawnchair/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lawnchair.xml \
-    vendor/lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lawnchair-whitelist.xml
+    $(LOCAL_PATH)/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LOCAL_PATH)/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lawnchair-whitelist.xml
 
 PRODUCT_PACKAGES += \
     QuickstepSwitcherOverlay \
     Lawnchair \
-    LawnchairOverlay \
     Lawnicons
+
+# Include package overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/common/
