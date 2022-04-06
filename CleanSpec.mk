06f4e253913cecfe9f510fe29427b2b724a8e6f9
Lynx <rifqikanekii140202@gmail.com>
2022-04-07 00:31:13 +0700

    Lawnchair: Fixup for android 10
    Signed-off-by: Lynx <rifqikanekii140202@gmail.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,2 +1,2 @@
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system_ext/priv-app/Lawnchair)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system_ext/app/Lawnicons)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/Lawnchair)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/app/Lawnicons)
